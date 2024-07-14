9b92d1b4ded20dbd49b7d003883242cad4e8e6ac
oussamanmh <mo_nemamcha@esi.dz>
2024-07-14 16:09:54 +0200

    first page

--- app/page.js
+++ app/page.js
@@ -2,7 +2,13 @@
 export default function Home() {
   return (
     <main className="">
-      <h1>my protfolio</h1>
+      <div className="flex flex-col items-center justify-center  gap-5">
+            <h1 className="text-4xl font-bold">I am Oussama Nemamcha <span className="text-purple-700 ">full stack developer</span></h1>
+            <p className="text-sm max-w-[500px] text-center ">
+              I am a full-stack developer specializing in both web and mobile applications. With expertise in HTML, CSS, JavaScript, Next.js, React, and Flutter, I create responsive, user-friendly digital solutions. My commitment to continuous learning ensures I stay updated with the latest trends and technologies, delivering modern, high-quality results.
+             </p>
+             <button className="bg-white pt-2 pb-2 pr-4 pl-4 border rounded-2xl">More about me </button>
+      </div>
     </main>
   );
 }
